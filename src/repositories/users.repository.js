ea6ffb22e3c6dacd1c46d001c5baadbd499eba98
황민도 <167045161+hwangmindo@users.noreply.github.com>
2024-06-18 18:33:07 +0900

    ✨ update : 회원가입 router, controller, service, repository 작성

---
+++ src/repositories/users.repository.js
@@ -0,0 +1,53 @@
+export class UsersRepository {
+  constructor(prisma) {
+    this.prisma = prisma;
+  }
+
+  createUsers = async ({
+    email,
+    hashedPassword,
+    name,
+    introduce,
+    profileImage,
+  }) => {
+    const data = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        introduce,
+        profileImage,
+      },
+      omit: {
+        password: true,
+      },
+    });
+
+    return data;
+  };
+
+  findOneEmail = async (email) => {
+    const data = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    return data;
+  };
+
+  findOneId = async (id) => {
+    const data = await this.prisma.user.findUnique({
+      where: { id },
+      omit: { password: true },
+    });
+
+    return data;
+  };
+
+  findOneRefreshTokenId = async (id) => {
+    const data = await this.prisma.refreshToken.findUnique({
+      where: { userId: id },
+    });
+
+    return data;
+  };
+}
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import { prisma } from '../utils/prisma.util.js';
+import { UsersRepository } from '../repositories/users.repository.js';
+import { AuthService } from '../services/auth.service.js';
+import { AuthController } from '../controllers/auth.controller.js';
+import { signupValidator } from '../middlewares/validators/sign-up-validator.middleware.js';
 
 const authRouter = express.Router();
+
+const usersRepository = new UsersRepository(prisma);
+const authService = new AuthService(usersRepository);
+const authController = new AuthController(authService);
+
+// 회원가입
+authRouter.post('/sign-up', signupValidator, authController.signUp)
 
 export { authRouter };
