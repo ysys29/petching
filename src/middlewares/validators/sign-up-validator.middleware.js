bac7cf9ce125422d4844be679686957e33cf94f7
황민도 <167045161+hwangmindo@users.noreply.github.com>
2024-06-18 12:11:53 +0900

    🎉 add : joi 스키마 생성

---
+++ src/middlewares/validators/sign-up-validator.middleware.js
@@ -0,0 +1,43 @@
+import Joi from 'joi';
+import { MESSAGES } from '../../constants/message.constant.js';
+
+export const signupValidator = async (req, res, next) => {
+  try {
+    const joiSchema = Joi.object({
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required()
+        .messages({
+          'string.email': MESSAGES.AUTH.COMMON.EMAIL.INVALID_FORMAT,
+          'any.required': MESSAGES.AUTH.COMMON.EMAIL.REQUIRED,
+        }),
+      password: Joi.string().required().min(6).max(12).messages({
+        'any.required': MESSAGES.AUTH.COMMON.PASSWORD.REQUIRED,
+        'string.base': MESSAGES.AUTH.COMMON.PASSWORD.NO_STRING,
+        'string.min': MESSAGES.AUTH.COMMON.PASSWORD.LENGTH,
+        'string.max': MESSAGES.AUTH.COMMON.PASSWORD.LENGTH,
+      }),
+      repeat_password: Joi.string()
+        .required()
+        .valid(Joi.ref('password'))
+        .messages({
+          'any.required': MESSAGES.AUTH.COMMON.REPEAT_PASSWORD.REQUIRED,
+          'any.only': MESSAGES.AUTH.COMMON.REPEAT_PASSWORD.NOT_MATCHED,
+        }),
+      name: Joi.string().required().messages({
+        'string.base': MESSAGES.AUTH.COMMON.NAME.NO_STRING,
+        'any.required': MESSAGES.AUTH.COMMON.NAME.REQUIRED,
+      }),
+      introduce: Joi.string().messages({
+        'string.base': MESSAGES.AUTH.COMMON.INTRODUCE.NO_STRING,
+      }),
+      profile_image: Joi.number().precision(1).messages({
+        'string.base': MESSAGES.AUTH.COMMON.PROFILE_IMAGE,
+      }),
+    });
+    await joiSchema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
