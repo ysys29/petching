4001b4c605274bc0b31afd49a4588ca572450848
황민도 <167045161+hwangmindo@users.noreply.github.com>
2024-06-19 12:00:21 +0900

    🩹 fix : tokens.js 코드 간단 수정

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -3,16 +3,17 @@ import {
   ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET,
 } from '../constants/env.constant.js';
+import { ACCESS_TOKEN_EXPIRES, REFRESH_TOKEN_EXPIRES } from '../constants/auth.constant.js';
 
 //엑세스 토큰 발급 함수
 export function createAccessToken({ id, role }) {
   return jwt.sign({ id, role }, ACCESS_TOKEN_SECRET, {
-    expiresIn: '12h',
+    expiresIn: ACCESS_TOKEN_EXPIRES,
   });
 }
 //리프레시 토큰 발급 함수
 export function createRefreshToken(id, role) {
   return jwt.sign({ id, role }, REFRESH_TOKEN_SECRET, {
-    expiresIn: '7d',
+    expiresIn: REFRESH_TOKEN_EXPIRES,
   });
 }
